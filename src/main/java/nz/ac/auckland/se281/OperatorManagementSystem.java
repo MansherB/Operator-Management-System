3d6d7bff97a6277618fdbc304bbddadb4469b269
Mansher Bhullar <mbhu385@aucklanduni.ac.nz>
2025-03-18 20:57:38 +1300

    Added T1_03 location full name using getFullName from Types

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -1,5 +1,7 @@
 package nz.ac.auckland.se281;
 
+import nz.ac.auckland.se281.Types.Location;
+
 public class OperatorManagementSystem {
 
   // Do not change the parameters of the constructor
@@ -10,7 +12,9 @@ public class OperatorManagementSystem {
   }
 
   public void createOperator(String operatorName, String location) {
-    MessageCli.OPERATOR_CREATED.printMessage(operatorName, location);
+    Location locationFound = Location.fromString(location);
+    String locationAsString = locationFound.getFullName();
+    MessageCli.OPERATOR_CREATED.printMessage(operatorName, "Something", locationAsString);
   }
 
   public void viewActivities(String operatorId) {
