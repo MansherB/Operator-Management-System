fac91fd7f7411f8ce0954462bf539c255cbc214c
Mansher Bhullar <mbhu385@aucklanduni.ac.nz>
2025-03-22 17:15:33 +1300

    Added abreviation of city using getLocationAbbreviation()

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -6,20 +6,30 @@ public class OperatorManagementSystem {
 
   // Do not change the parameters of the constructor
   public OperatorManagementSystem() {}
-
+  
   public void searchOperators(String keyword) {
     MessageCli.OPERATORS_FOUND.printMessage("are", "no", "s", ".");
   }
 
   public void createOperator(String operatorName, String location) {
+    // Getting location from Types.java 
     Location locationFound = Location.fromString(location);
+    // Assigning full english and maori name
     String locationAsString = locationFound.getFullName();
+    // Using split to split up the Operator words in the array
     String[] words = operatorName.split(" ");
+
+    String abbreviationnAsString = locationFound.getLocationAbbreviation();
+
     String wordsAsString = "";
 
+    // Looping through each word in the array and retreiving the first letter (index 0)
     for (String word : words) {
-      wordsAsString += word.charAt(0);
-}
+      wordsAsString += word.charAt(0); 
+  }   
+
+    wordsAsString = wordsAsString + "-" + abbreviationnAsString; 
+    
     MessageCli.OPERATOR_CREATED.printMessage(operatorName, wordsAsString, locationAsString);
   }
 
@@ -67,3 +77,5 @@ public class OperatorManagementSystem {
     // TODO implement
   }
 }
+
+
